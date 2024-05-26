a8e0f6c4da9bd3c96826c70904517230b5c799dd
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-05-26 11:08:19 +0000

    feat: created

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,150 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
+import { Show, createEffect, createSignal, onMount } from 'solid-js'
 import './App.css'
+import { LOCAL_STORAGE_KEYS, SYSTEM_PROMPT } from './constants'
+import robot from '../assets/robot.png'
+import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from '@google/generative-ai'
 
 function App() {
-  const [count, setCount] = createSignal(0)
+  const [token, setToken] = createSignal(
+    localStorage.getItem(LOCAL_STORAGE_KEYS.GEMINI_API_KEY) ?? '',
+  )
+  let file: Blob | undefined = void 0
+  const [getImageUrl, setImageUrl] = createSignal('')
+  const [generatedText, setGeneratedText] = createSignal('')
+  createEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEYS.GEMINI_API_KEY, token())
+  })
 
+  const generate = async () => {
+    if (!file) {
+      return
+    }
+    const b64 = await new Promise<string>((resolve) => {
+      const reader = new FileReader()
+      reader.onload = () => {
+        resolve(
+          (reader.result as string).replace(/^data:image\/.+?;base64,/, ''),
+        )
+      }
+      reader.readAsDataURL(file as Blob)
+    })
+
+    const genAI = new GoogleGenerativeAI(token())
+    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
+    const chat = model.startChat({
+      generationConfig: {
+        maxOutputTokens: 100,
+        temperature: 0.5
+      },
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+        }
+      ]
+    })
+    const result = await chat.sendMessage([
+      {
+        text: SYSTEM_PROMPT,
+      },
+      {
+        inlineData: {
+          data: b64,
+          mimeType: file.type,
+        },
+      },
+    ])
+    const res = result.response.text()
+    console.log(res)
+    setGeneratedText(res)
+  }
   return (
     <>
+    <div class="flex flex-col h-[100dvh]">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
+        <label>
+          Enter Gemini API Key:
+          <input
+            class="border p-1 m-1 rounded-full"
+            placeholder="xxxx-xxxx"
+            type="password"
+            value={token()}
+            onInput={(e) => setToken(e.target.value)}
+          />
+        </label>
+        <div class="flex justify-around">
+          <button
+            type="button"
+            class="p-2 rounded-full bg-blue-500 text-white"
+            onClick={() => {
+              const inp = document.createElement('input')
+              inp.type = 'file'
+              inp.onchange = () => {
+                file = inp?.files?.[0]
+                if (file) {
+                  const url = URL.createObjectURL(file)
+                  setImageUrl(url)
+                }
+                inp.remove()
+              }
+              inp.click()
+            }}
+          >
+            ファイルを選択
+          </button>
+          <button
+            type="button"
+            class="rounded-full p-2 bg-blue-500 text-white disabled:opacity-30"
+            disabled={!getImageUrl()}
+            onClick={() => {
+              generate()
+            }}
+          >
+            生成
+          </button>
+        </div>
       </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div class="grow h-full">
+        <Show when={!!getImageUrl()}>
+          <div class="relative w-full h-full">
+            <div class="h-full">
+              <img src={getImageUrl()} alt="target" class="max-w-full max-h-full" />
+            </div>
+            <div class="absolute right-0 bottom-0">
+              <img src={robot} alt="target" />
+              <div
+                class="absolute text-center text-2xl grid place-items-center font-bold"
+                style={{
+                  //  bottom: '500px',
+                  right: '70px',
+                  top: '50px',
+                  height: '200px',
+                  width: '400px',
+                }}
+              >
+                <div>{generatedText()}</div>
+              </div>
+            </div>
+          </div>
+        </Show>
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
+      </div>
     </>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
 
-import './index.css'
 import App from './App'
 
+if (import.meta.env.DEV) {
+    const scr = document.createElement('script')
+    scr.onload = () => {
+      // @ts-expect-error
+      globalThis.eruda.init()
+    }
+    scr.src = '//cdn.jsdelivr.net/npm/eruda'
+    document.head.append(scr)
+}
 const root = document.getElementById('root')
 
-render(() => <App />, root!)
+render(() => <App />, root as HTMLDivElement)
